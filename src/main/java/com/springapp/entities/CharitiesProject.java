f773d43e6085e01c0ed189ac8c252c023faf5146
DevTulio <mikeavalardo1@hotmail.com>
2013-12-06 22:54:56 +0800

    added CharitiesProject Classes

---
+++ src/main/java/com/springapp/entities/CharitiesProject.java
@@ -0,0 +1,13 @@
+package com.springapp.entities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Angela
+ * Date: 12/6/13
+ * Time: 10:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CharitiesProject {
+
+
+}
