35956bde45d9bec0bdfbbbc15749c083548192a4
DevTulio <mikeavalardo1@hotmail.com>
2013-12-08 16:19:58 +0800

    added CharitiesProject Classes,Hibernate

--- src/main/java/com/springapp/entities/CharitiesProject.java
+++ src/main/java/com/springapp/entities/CharitiesProject.java
@@ -1,4 +1,5 @@
 package com.springapp.entities;
+import javax.persistence.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,7 +8,40 @@ package com.springapp.entities;
  * Time: 10:53 PM
  * To change this template use File | Settings | File Templates.
  */
+@Entity(name="charitiesProject")
 public class CharitiesProject {
+    @Basic
+     private String projectName;
+    @Basic
+     private String projectDetails;
+    @Basic
+     private Double projectBudgetNeed;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDetails() {
+        return projectDetails;
+    }
+
+    public void setProjectDetails(String projectDetails) {
+        this.projectDetails = projectDetails;
+    }
+
+    public Double getProjectBudgetNeed() {
+        return projectBudgetNeed;
+    }
+
+    public void setProjectBudgetNeed(Double projectBudgetNeed) {
+        this.projectBudgetNeed = projectBudgetNeed;
+    }
+
+
 
 
 }
