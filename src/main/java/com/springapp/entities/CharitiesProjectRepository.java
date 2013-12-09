ca12ba4a8bb825496bbfaf2ed2ee393b3170824f
DevTulio <mikeavalardo1@hotmail.com>
2013-12-09 21:46:42 +0800

    added ChraitiesProjectRepository(Spring Data JPA)

---
+++ src/main/java/com/springapp/entities/CharitiesProjectRepository.java
@@ -0,0 +1,16 @@
+package com.springapp.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Angela
+ * Date: 12/9/13
+ * Time: 9:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public interface CharitiesProjectRepository extends JpaRepository<CharitiesProject,Long> {
+
+}
+
